ec8cadf12b1f73239d910c75ff4acb61217e7ec0
Will Gaybrick <william.gaybrick@gmail.com>
2011-11-16 19:55:44 -0500

    Added date formatting method

--- models/post.js
+++ models/post.js
@@ -7,4 +7,11 @@ var postSchema = new Schema( {
 	,	date    : { type: Date, default: Date.now }
 } );
 
+postSchema.methods.date_display = function ( cb ) {
+	var date = this.date.getDate();
+	var month = this.date.getMonth() + 1;
+	var year = this.date.getFullYear();
+	var dateString = month + "/" + date + "/" + year;
+	return dateString;
+};
 module.exports = mongoose.model( 'Post', postSchema );
