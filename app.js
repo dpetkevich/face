b82e1ead54ace5966650da1ffcd9738067a05770
Will Gaybrick <william.gaybrick@gmail.com>
2011-11-16 19:38:00 -0500

    Revert "Integrated mongo database"
    This reverts commit f83b14b72c04f92bb626738457becf00e5ba8cca.

--- app.js
+++ app.js
@@ -3,40 +3,36 @@
  * Module dependencies.
  */
 
-var express = require( 'express' )
-  , routes = require( './routes' )
-	, mongoose = require( 'mongoose' );
-
-mongoose.connect('mongodb://localhost/crushFlow');
+var express = require('express')
+  , routes = require('./routes')
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function () {
-  app.set( 'views' , __dirname + '/views' );
-  app.set( 'view engine', 'ejs' );
-  app.use( express.bodyParser() );
-  app.use( express.methodOverride() );
-  app.use( app.router );
-  app.use( express.static(__dirname + '/public') );
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-app.configure( 'development', function () {
-  app.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) ); 
-} );
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
 
-app.configure( 'production', function () {
-  app.use(express.errorHandler() ); 
-} );
-
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
 
 // Routes
-app.get( '/', routes.index );
-app.post( '/', routes.new_post );
+
+app.get('/', routes.index);
 
 
 var port = process.env.PORT || 3000;
 
 app.listen( port );
-console.log( "Express server listening on port %d in %s mode", app.address().port, app.settings.env );
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
